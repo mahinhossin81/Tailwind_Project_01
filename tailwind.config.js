7a4977d86a487227f9277589b76ba3220323b47e
mahinhossin81 <mahinhossin81@mail.com>
2024-07-03 08:20:04 +0600

    tailwind project

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'leagure': ["League Spartan", 'sans-serif']
+      },
+    },
+  },
+  plugins: [],
+}
+
