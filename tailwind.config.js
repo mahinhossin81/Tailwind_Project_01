e25505421d7346fdac31770ad9f20df731f0e6d5
mahinhossin81 <mahinhossin81@mail.com>
2024-07-03 10:08:22 +0600

    bg banner

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,9 @@ module.exports = {
   content: ["*"],
   theme: {
     extend: {
+      backgroundImage: {
+        banner: "url('/images/pexels-photo-8370752 1.png')"
+      },
       fontFamily: {
         'leagure': ["League Spartan", 'sans-serif']
       },
